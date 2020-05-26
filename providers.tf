cc599fd348d8eb1621a6e1d2d10aefe5d5a279ea
Rahul Kumar <45115618+itrahulit@users.noreply.github.com>
2020-05-26 23:03:10 +0800

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "ap-southeast-1"
-	access_key = "AKIAXWXKWTHK2IPFGWHE"
-	secret_key = "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"
+	access_key = ""
+	secret_key = ""
 }
